a5db9eda8772a695abd3e766c12c18199e850a43
Majorrehan18 <rkmoyal18@gmail.com>
2026-08-26 16:22:12 +0530

    Refactor

--- Arsuu.c
+++ Arsuu.c
@@ -1,8 +1,18 @@
-intmain()
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
 {
-float A,B,C;
-A=4;
-B=5;
-C=A+B;
-printf("%f",C);
+    float n3, n4, Sum;
+    printf("Enter first number: ");
+    scanf("%f", &n3);
+    printf("Enter second number: ");
+    scanf("%f", &n4);
+    Sum=n3+n4;
+
+    printf("The Sum is: %.2f\n", Sum);
+    printf("Press Enter to exit...");
+    getchar();
+    getchar(); // Waits for the user to press Enter
 }
