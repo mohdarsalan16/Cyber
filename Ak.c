6ac725bdb488f031b12ce544d068701ecdbbb4f6
Mohammed Arsalan <mohammedarsalan7849@gmail.com>
2026-08-27 16:01:04 +0530

    N

--- Ak.c
+++ Ak.c
@@ -1,9 +1,13 @@
-main()
+#include <stdio.h>
+
+int main() 
 {
-int A,B,C;
-A=5;
-B=15;
-C=A+B;
-printf("add");
-printf("%d",C);
+    int A, B, C;
+    A = 5;
+    B = 15;
+    C = A + B;
+    printf("add\n");
+    printf("%d\n", C);
+    return 0;
 }
+
